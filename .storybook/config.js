75aeb7981510f9bb8c807aec8364cb2464490cc2
zhangweijun <ahnqihyun@sina.cn>
2019-05-26 11:03:11 +0800

    init

---
+++ .storybook/config.js
@@ -0,0 +1,9 @@
+import {configure} from '@storybook/react';
+function loadStories() {
+    require('../stories/index.js');
+
+    // const req = require.context('../stories', true, /\.stories\.js$/);
+    // req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
---
+++ stories/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import App from '../src/App';
+
+storiesOf('App', module)
+  .add('Home', () => (
+    <App />
+  ))
+ 
